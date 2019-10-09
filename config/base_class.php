c8b4e08251689bf4778ae8110f254c0826928071
luke munnich <lukemunnich@gmail.com>
2019-10-09 15:30:26 +0200

    ädded config folder

---
+++ config/base_class.php
@@ -0,0 +1,10 @@
+<?php
+//name of the server
+define('DB_HOST_CLASS','localhost');
+//Name of the System Admo
+define('DB_USERNAME_CLASS','root');
+//name of the Server password
+define('DB_PASS_CLASS','');
+//name of the database to connect - to_do_database. 
+define('DB_NAME_CLASS','navtodo');
+?>
